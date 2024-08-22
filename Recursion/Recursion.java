48e1bdd5ed10c38201d4f2adc5595ba1ee2c1239
Dinushasl <dinushanawarathna62@gmail.com>
2024-08-22 23:29:07 +0530

    Ok

---
+++ Recursion/Recursion.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class Recursion {
+	
+
+	   
+	    public static int factorial(int n) {
+	        
+	        if (n == 0) {
+	            return 1;
+	        }
+	       
+	        return n * factorial(n - 1);
+	    }
+
+	    public static void main(String[] args) {
+	        int number = 5;
+	        int result = factorial(number);
+	        System.out.println("Factorial of " + number + " is: " + result);
+	    }
+	}
+
+
+
